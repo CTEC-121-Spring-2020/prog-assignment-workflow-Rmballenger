1ce15adbfcb6ce62c38a5cd478d4279c51f3de64
Rmballenger <robert.ballenger@live.com>
2020-04-27 10:58:01 -0700

    Last Attempt to fix this.

---
+++ Doingitagain.py
@@ -0,0 +1,21 @@
+"""
+CTEC 121
+Robert Ballenger
+Task: Programming Assignment
+Add in my name and the other information here, add print statements, exc.
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+
+def main():
+    print("Hello World! It's one of my favorite things to see accidentally uploaded to high class websites and such.")
+    print("Jedi Python language. Reason for the jedi part?")
+    print("Here is a third statment, is this a cop out of an answer?  Could be, but I'm not very good at ASCII so.... You getrambling text!")
+
+
+main()
